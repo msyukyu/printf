3cd0182497d1a50849f9ba8e954c015adeec21e3
Damien Abeloos <dabeloos@w-r8-p3.s19.be>
2019-01-11 19:47:27 +0100

    clean free

--- printf.c
+++ printf.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 14:56:39 by dabeloos          #+#    #+#             */
-/*   Updated: 2019/01/11 19:33:42 by dabeloos         ###   ########.fr       */
+/*   Updated: 2019/01/11 19:46:28 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char			*str_lst_join(t_str *slst, size_t len)
 	char		*out;
 	char		*cur;
 	t_str		*head;
+	t_str		*prev;
 
 	if (!slst || !(out = (char*)malloc(sizeof(char) * len)))
 		return (NULL);
@@ -26,7 +27,11 @@ char			*str_lst_join(t_str *slst, size_t len)
 	{
 		cur = ft_strncpy(cur, (const char*)head->txt, head->len);
 		cur += head->len;
+		prev = head;
 		head = head->next;
+		if (!prev->is_raw)
+			free(prev->txt);
+		free(prev);
 	}
 	return (out);
 }
@@ -44,6 +49,7 @@ int				ft_printf(const char *format, ...)
 	out = str_lst_join(slst, len);
 	write(1, out, len);
 	va_end(ap);
+	free(out);
 	return (len);
 }
 
